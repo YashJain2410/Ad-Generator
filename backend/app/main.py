c4a60e923f87ef4093d4c1be2aa07284bedb77fa
Yash Jain <jainy5855@gmail.com>
2026-03-01 20:44:48 +0530

    Password hashing, User registration and User login added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from .routers import auth
+
+from .db.session import engine
+from .models import user
 
 app = FastAPI(title="AI Ad Generator API")
 
 @app.get("/")
 def root():
     return {"message" : "API Running"}
+
+app.include_router(auth.router)
+
+user.Base.metadata.create_all(bind=engine)
---
+++ backend/app/routers/auth.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..schemas.user import UserCreate, UserLogin, Token
+from ..models.user import User
+from ..core.security import hash_password, verify_password, create_access_token
+from ..db.session import get_db
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/register", response_model=Token)
+def register(user_data: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user_data.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    new_user = User(
+        name=user_data.name,
+        email=user_data.email,
+        password_hash=hash_password(user_data.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    token = create_access_token({"sub": str(new_user.id)})
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.post("/login", response_model=Token)
+def login(user_data: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == user_data.email).first()
+
+    if not user or not verify_password(user_data.password, user.password_hash):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    token = create_access_token({"sub": str(user.id)})
+    return {"access_token": token, "token_type": "bearer"}
